7ffcfee04e4b3feab917fc000c31c1d80e54707f
Siddharth sharma <mynmss03@gmail.com>
2024-08-01 18:34:01 +0530

    api

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.get('/iocompiler/termux', (req, res) => {
+    res.send("Hello");
+  });
+
 // Serve the install.sh file at the specified URL
 app.get('/iocompiler/termux', (req, res) => {
   res.sendFile(path.join(__dirname, 'install.sh'));

7e5cc809def777ab202346a6fcf7742ccf731b1f
Siddharth sharma <mynmss03@gmail.com>
2024-08-21 16:20:17 +0530

    vercel

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello");
-  });
+  res.send('Hello');
+});
 
 // Serve the install.sh file at the specified URL
 app.get('/iocompiler/termux.sh', (req, res) => {
   res.sendFile(path.join(__dirname, 'install.sh'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+module.exports = app;

f0a522e623b3e76856d52d6d7eb083dfcb2c5799
Siddharth sharma <mynmss03@gmail.com>
2024-08-21 16:26:15 +0530

    vercel

--- api/index.js
+++ api/index.js
@@ -6,9 +6,4 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-// Serve the install.sh file at the specified URL
-app.get('/iocompiler/termux.sh', (req, res) => {
-  res.sendFile(path.join(__dirname, 'install.sh'));
-});
-
 module.exports = app;
